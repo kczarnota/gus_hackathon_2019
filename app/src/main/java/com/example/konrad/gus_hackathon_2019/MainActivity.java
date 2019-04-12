ea3ce2fef480c447a28973b86a91bb305112ee03
Jakub Lipinski <lipinski.jb@gmail.com>
2019-04-12 18:23:44 +0200

    dodanie faba

--- app/src/main/java/com/example/konrad/gus_hackathon_2019/MainActivity.java
+++ app/src/main/java/com/example/konrad/gus_hackathon_2019/MainActivity.java
@@ -1,16 +1,27 @@
 package com.example.konrad.gus_hackathon_2019;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton mFloatingActionButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFloatingActionButton = findViewById(R.id.floatingActionButton);
+        mFloatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("xxx", "FUB clicked");
+            }
+        });
 
     }
 }
