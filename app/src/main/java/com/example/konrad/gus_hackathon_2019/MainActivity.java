84d73922aa72f16b88f020eb1c77c72867f363b2
Jakub Lipinski <lipinski.jb@gmail.com>
2019-04-12 18:52:51 +0200

    dev

--- app/src/main/java/com/example/konrad/gus_hackathon_2019/MainActivity.java
+++ app/src/main/java/com/example/konrad/gus_hackathon_2019/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.konrad.gus_hackathon_2019;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mRankingBtn;
+    private Button mInstructionBtn;
+    private ImageButton mCameraBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRankingBtn = findViewById(R.id.rankingBtn);
+        mCameraBtn = findViewById(R.id.cameraBtn);
+        mInstructionBtn = findViewById(R.id.instructionBtn);
+
+        mRankingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mCameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mInstructionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
 
     }
 }
