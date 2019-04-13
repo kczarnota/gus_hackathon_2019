a4ab14d7a81ed7d00f1d20f62d59164535c347cb
Jakub Lipinski <lipinski.jb@gmail.com>
2019-04-13 14:34:36 +0200

    extract strings

--- app/src/main/java/com/example/konrad/gus_hackathon_2019/data/Person.java
+++ app/src/main/java/com/example/konrad/gus_hackathon_2019/data/Person.java
@@ -5,6 +5,8 @@ import android.content.SharedPreferences;
 import android.view.Gravity;
 import android.widget.Toast;
 
+import com.example.konrad.gus_hackathon_2019.R;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,10 +17,12 @@ public class Person {
     private List<Boolean> mAchievements = new ArrayList<>();
     private Context mContext;
 
+    public final static String STATISTIC = "statystyk";
+
     public Person(Context context) {
         mSharedPrefs = context.getSharedPreferences("Person", Context.MODE_PRIVATE);
         mPoints = mSharedPrefs.getInt("points", 0);
-        mAchievements.add(mSharedPrefs.getBoolean("0", true));
+        mAchievements.add(mSharedPrefs.getBoolean("0", false));
         mAchievements.add(mSharedPrefs.getBoolean("1", false));
         mAchievements.add(mSharedPrefs.getBoolean("2", false));
         mAchievements.add(mSharedPrefs.getBoolean("3", false));
@@ -91,8 +95,7 @@ public class Person {
             case "bottle":
                 if (!mAchievements.get(0)) {
                     setAchieved(0);
-                    Toast toast = Toast.makeText(mContext, "\uD83C\uDFC6 Znalazłeś butelkę! \uD83C\uDFC6",
-                            Toast.LENGTH_LONG);
+                    Toast toast = Toast.makeText(mContext, R.string.found_bottle, Toast.LENGTH_LONG);
                     toast.setGravity(Gravity.BOTTOM, 0, 400);
                     toast.show();
                 }
@@ -100,8 +103,7 @@ public class Person {
             case "person":
                 if (!mAchievements.get(1)) {
                     setAchieved(1);
-                    Toast toast = Toast.makeText(mContext, "\uD83C\uDFC6 Znalazłeś osobę! \uD83C\uDFC6",
-                            Toast.LENGTH_LONG);
+                    Toast toast = Toast.makeText(mContext, R.string.found_person, Toast.LENGTH_LONG);
                     toast.setGravity(Gravity.BOTTOM, 0, 400);
                     toast.show();
                 }
@@ -109,8 +111,7 @@ public class Person {
             case "clock":
                 if (!mAchievements.get(2)) {
                     setAchieved(2);
-                    Toast toast = Toast.makeText(mContext, "\uD83C\uDFC6 Znalazłeś zegar! \uD83C\uDFC6",
-                            Toast.LENGTH_LONG);
+                    Toast toast = Toast.makeText(mContext, R.string.found_clock, Toast.LENGTH_LONG);
                     toast.setGravity(Gravity.BOTTOM, 0, 400);
                     toast.show();
                 }
@@ -118,8 +119,7 @@ public class Person {
             case "cell phone":
                 if (!mAchievements.get(3)) {
                     setAchieved(3);
-                    Toast toast = Toast.makeText(mContext, "\uD83C\uDFC6 Znalazłeś telefon! \uD83C\uDFC6",
-                            Toast.LENGTH_LONG);
+                    Toast toast = Toast.makeText(mContext, R.string.found_phone, Toast.LENGTH_LONG);
                     toast.setGravity(Gravity.BOTTOM, 0, 400);
                     toast.show();
                 }
@@ -127,8 +127,7 @@ public class Person {
             case "laptop":
                 if (!mAchievements.get(4)) {
                     setAchieved(4);
-                    Toast toast = Toast.makeText(mContext, "\uD83C\uDFC6 Znalazłeś laptop! \uD83C\uDFC6",
-                            Toast.LENGTH_LONG);
+                    Toast toast = Toast.makeText(mContext, R.string.found_laptop, Toast.LENGTH_LONG);
                     toast.setGravity(Gravity.BOTTOM, 0, 400);
                     toast.show();
                 }
@@ -136,18 +135,16 @@ public class Person {
             case "cat":
                 if (!mAchievements.get(5)) {
                     setAchieved(5);
-                    Toast toast = Toast.makeText(mContext, "\uD83C\uDFC6 Znalazłeś kota! \uD83C\uDFC6",
-                            Toast.LENGTH_LONG);
+                    Toast toast = Toast.makeText(mContext, R.string.found_cat, Toast.LENGTH_LONG);
                     toast.setGravity(Gravity.BOTTOM, 0, 400);
                     toast.show();
                 }
                 break;
-            case "statystyk":
+            case STATISTIC:
                 if (!mAchievements.get(6)) {
                     setAchieved(6);
                     addPoints(90);
-                    Toast toast = Toast.makeText(mContext, "\uD83C\uDFC6 Jestes Statystykiem! \uD83C\uDFC6",
-                            Toast.LENGTH_LONG);
+                    Toast toast = Toast.makeText(mContext, R.string.statystyk, Toast.LENGTH_LONG);
                     toast.setGravity(Gravity.BOTTOM, 0, 400);
                     toast.show();
                 }
