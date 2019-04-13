76f73dd5ff749141aecb563aa6726e53197c515d
TwistedAnckle <twisted.anckle@outlook.com>
2019-04-13 14:54:55 +0200

    added wav sound when achievement has been unlocked

--- app/src/main/java/com/example/konrad/gus_hackathon_2019/data/Person.java
+++ app/src/main/java/com/example/konrad/gus_hackathon_2019/data/Person.java
@@ -2,11 +2,14 @@ package com.example.konrad.gus_hackathon_2019.data;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
 import android.view.Gravity;
 import android.widget.Toast;
 
 import com.example.konrad.gus_hackathon_2019.R;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -95,60 +98,76 @@ public class Person {
             case "bottle":
                 if (!mAchievements.get(0)) {
                     setAchieved(0);
-                    Toast toast = Toast.makeText(mContext, R.string.found_bottle, Toast.LENGTH_LONG);
-                    toast.setGravity(Gravity.BOTTOM, 0, 400);
-                    toast.show();
+                    notifyAchievementUnlocked(R.string.found_bottle);
                 }
                 break;
             case "person":
                 if (!mAchievements.get(1)) {
                     setAchieved(1);
-                    Toast toast = Toast.makeText(mContext, R.string.found_person, Toast.LENGTH_LONG);
-                    toast.setGravity(Gravity.BOTTOM, 0, 400);
-                    toast.show();
+                    notifyAchievementUnlocked(R.string.found_person);
                 }
                 break;
             case "clock":
                 if (!mAchievements.get(2)) {
                     setAchieved(2);
-                    Toast toast = Toast.makeText(mContext, R.string.found_clock, Toast.LENGTH_LONG);
-                    toast.setGravity(Gravity.BOTTOM, 0, 400);
-                    toast.show();
+                    notifyAchievementUnlocked(R.string.found_clock);
                 }
                 break;
             case "cell phone":
                 if (!mAchievements.get(3)) {
                     setAchieved(3);
-                    Toast toast = Toast.makeText(mContext, R.string.found_phone, Toast.LENGTH_LONG);
-                    toast.setGravity(Gravity.BOTTOM, 0, 400);
-                    toast.show();
+                    notifyAchievementUnlocked(R.string.found_phone);
                 }
                 break;
             case "laptop":
                 if (!mAchievements.get(4)) {
                     setAchieved(4);
-                    Toast toast = Toast.makeText(mContext, R.string.found_laptop, Toast.LENGTH_LONG);
-                    toast.setGravity(Gravity.BOTTOM, 0, 400);
-                    toast.show();
+                    notifyAchievementUnlocked(R.string.found_laptop);
                 }
                 break;
             case "cat":
                 if (!mAchievements.get(5)) {
                     setAchieved(5);
-                    Toast toast = Toast.makeText(mContext, R.string.found_cat, Toast.LENGTH_LONG);
-                    toast.setGravity(Gravity.BOTTOM, 0, 400);
-                    toast.show();
+                    notifyAchievementUnlocked(R.string.found_cat);
                 }
                 break;
             case STATISTIC:
                 if (!mAchievements.get(6)) {
                     setAchieved(6);
                     addPoints(90);
-                    Toast toast = Toast.makeText(mContext, R.string.statystyk, Toast.LENGTH_LONG);
-                    toast.setGravity(Gravity.BOTTOM, 0, 400);
-                    toast.show();
+                    notifyAchievementUnlocked(R.string.statystyk);
                 }
                 break;
         }
+    }
+
+    private void notifyAchievementUnlocked(int messageId) {
+        Toast toast = Toast.makeText(mContext, messageId, Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.BOTTOM, 0, 400);
+        toast.show();
+
+        startSound("NFF-coin-04.wav"); // more free sounds can be found at NoiseForFun.com
+    }
+
+    private void startSound(String filename) {
+        AssetFileDescriptor afd = null;
+        try {
+            afd = mContext.getResources().getAssets().openFd(filename);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        MediaPlayer player = new MediaPlayer();
+        try {
+            assert afd != null;
+            player.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            player.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        player.start();
     }
 }
