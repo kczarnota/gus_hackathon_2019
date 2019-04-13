bd7cde1181dbded225a684b506cd768afbf94c3a
Jakub Lipinski <lipinski.jb@gmail.com>
2019-04-13 12:07:08 +0200

    dev

--- app/src/main/java/com/example/konrad/gus_hackathon_2019/MainActivity.java
+++ app/src/main/java/com/example/konrad/gus_hackathon_2019/MainActivity.java
@@ -69,67 +69,67 @@ public class MainActivity extends AppCompatActivity {
                     if (achievements.get(i)) {
                         mBinding.trophy1.setText("Zdobyte");
                         mBinding.trophy1.setCompoundDrawablesRelativeWithIntrinsicBounds(null,
-                                getDrawable(R.drawable.ic_trophy_gold), null, null);
+                                getDrawable(R.drawable.ic_water), null, null);
                         mEyeClickedCounter = 5;
                     } else {
                         mBinding.trophy1.setText("");
                         mBinding.trophy1.setCompoundDrawablesRelativeWithIntrinsicBounds(null,
-                                getDrawable(R.drawable.ic_trophy), null, null);
+                                getDrawable(R.drawable.ic_water_sec), null, null);
                     }
                     break;
                 case 1:
                     if (achievements.get(i)) {
                         mBinding.trophy2.setText("Zdobyte");
                         mBinding.trophy2.setCompoundDrawablesRelativeWithIntrinsicBounds(null,
-                                getDrawable(R.drawable.ic_trophy_gold), null, null);
+                                getDrawable(R.drawable.ic_person), null, null);
                     } else {
                         mBinding.trophy2.setText("");
                         mBinding.trophy2.setCompoundDrawablesRelativeWithIntrinsicBounds(null,
-                                getDrawable(R.drawable.ic_trophy), null, null);
+                                getDrawable(R.drawable.ic_person_sec), null, null);
                     }
                     break;
                 case 2:
                     if (achievements.get(i)) {
                         mBinding.trophy3.setText("Zdobyte");
                         mBinding.trophy3.setCompoundDrawablesRelativeWithIntrinsicBounds(null,
-                                getDrawable(R.drawable.ic_trophy_gold), null, null);
+                                getDrawable(R.drawable.ic_clock), null, null);
                     } else {
                         mBinding.trophy3.setText("");
                         mBinding.trophy3.setCompoundDrawablesRelativeWithIntrinsicBounds(null,
-                                getDrawable(R.drawable.ic_trophy), null, null);
+                                getDrawable(R.drawable.ic_clock_sec), null, null);
                     }
                     break;
                 case 3:
                     if (achievements.get(i)) {
                         mBinding.trophy4.setText("Zdobyte");
                         mBinding.trophy4.setCompoundDrawablesRelativeWithIntrinsicBounds(null,
-                                getDrawable(R.drawable.ic_trophy_gold), null, null);
+                                getDrawable(R.drawable.ic_smartphone), null, null);
                     } else {
                         mBinding.trophy4.setText("");
                         mBinding.trophy4.setCompoundDrawablesRelativeWithIntrinsicBounds(null,
-                                getDrawable(R.drawable.ic_trophy), null, null);
+                                getDrawable(R.drawable.ic_smartphone_sec), null, null);
                     }
                     break;
                 case 4:
                     if (achievements.get(i)) {
                         mBinding.trophy5.setText("Zdobyte");
                         mBinding.trophy5.setCompoundDrawablesRelativeWithIntrinsicBounds(null,
-                                getDrawable(R.drawable.ic_trophy_gold), null, null);
+                                getDrawable(R.drawable.ic_laptop), null, null);
                     } else {
                         mBinding.trophy5.setText("");
                         mBinding.trophy5.setCompoundDrawablesRelativeWithIntrinsicBounds(null,
-                                getDrawable(R.drawable.ic_trophy), null, null);
+                                getDrawable(R.drawable.ic_laptop_sec), null, null);
                     }
                     break;
                 case 5:
                     if (achievements.get(i)) {
                         mBinding.trophy6.setText("Zdobyte");
                         mBinding.trophy6.setCompoundDrawablesRelativeWithIntrinsicBounds(null,
-                                getDrawable(R.drawable.ic_trophy_gold), null, null);
+                                getDrawable(R.drawable.ic_cat), null, null);
                     } else {
                         mBinding.trophy6.setText("");
                         mBinding.trophy6.setCompoundDrawablesRelativeWithIntrinsicBounds(null,
-                                getDrawable(R.drawable.ic_trophy), null, null);
+                                getDrawable(R.drawable.ic_cat_sec), null, null);
                     }
                     break;
             }
